f891d0fb89e2463284b1e7b9ad02f01f297049b0
zhouzhou19950825 <729535800@QQ.com>
2018-05-09 07:07:03 +0800

    config

--- upic-spring-test/src/main/java/com/upic/config/RouteConfug.java
+++ upic-spring-test/src/main/java/com/upic/config/RouteConfug.java
@@ -1,9 +1,13 @@
 package com.upic.config;
 
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +17,6 @@ import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import com.upic.handler.PersonHandler;
-import com.upic.service.UserService;
 
 @Configuration
 public class RouteConfug {
@@ -21,6 +24,23 @@ public class RouteConfug {
 	@Autowired
 	PersonHandler handler;
 
+	private List<String> allowUrl=new ArrayList<String>();
+	
+	public RouteConfug() {
+		allowUrl.add("/person");
+	}
+	/**
+	 * RequestPredicate.and(RequestPredicate) - 两者都必须匹配。
+	 * 
+	 * RequestPredicate.or(RequestPredicate) - 可能匹配。
+	 * 
+	 * RouterFunction.and(RouterFunction)
+	 * 
+	 * RouterFunction.andRoute(RequestPredicate, HandlerFunction) -
+	 * RouterFunction.and()嵌套的快捷方式 RouterFunctions.route()。
+	 * 
+	 * @return
+	 */
 	@Bean
 	public RouterFunction<ServerResponse> moreMethod() {
 		RouterFunction<ServerResponse> route = RouterFunctions
@@ -28,7 +48,25 @@ public class RouteConfug {
 				.andRoute(GET("/person").and(accept(APPLICATION_JSON)), handler::listPeople)
 				.andRoute(POST("/person"), handler::createPerson)
 				.andRoute(GET("/getJsonPerson/{id}").and(accept(APPLICATION_JSON)), handler::getJsonPerson);
-
-		return route;
+		// 加一个filter
+		// route.filter((request, next) -> {
+		// if (securityManager.allowAccessTo(request.path())) {
+		// return next.handle(request);
+		// } else {
+		// return ServerResponse.status(UNAUTHORIZED).build();
+		// }
+		// });
+		
+		//增加了过滤器
+		RouterFunction<ServerResponse> filter = route.filter((request, next) -> {
+			for(String url :allowUrl) {
+				if(request.path().startsWith(url)) {
+					return next.handle(request);
+				}
+			}
+			return ServerResponse.status(UNAUTHORIZED).build();
+		});
+		return filter;
 	}
+
 }
--- upic-spring-test/src/main/java/com/upic/config/WebConfig.java
+++ upic-spring-test/src/main/java/com/upic/config/WebConfig.java
@@ -1,23 +1,46 @@
-//package com.upic.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.reactive.config.EnableWebFlux;
-//import org.springframework.web.reactive.config.UrlBasedViewResolverRegistration;
-//import org.springframework.web.reactive.config.ViewResolverRegistry;
-//import org.springframework.web.reactive.config.WebFluxConfigurer;
-///**
-// * dtz
-// * @author dtz
-// * @date 2018年4月11日
-// * Email upicdtz@qq.com
-// */
-//@Configuration
-//@EnableWebFlux
-//public class WebConfig implements WebFluxConfigurer {
-//
-//    @Override
-//    public void configureViewResolvers(ViewResolverRegistry registry) {
-//        // ...
-//    	UrlBasedViewResolverRegistration freeMarker = registry.freeMarker();
-//    }
-//}
+package com.upic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.ViewResolverRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+
+	// @Bean
+	// public RouterFunction<?> routerFunctionA() {
+	// RouterFunction<ServerResponse> route = RouterFunctions
+	// .route(GET("/person/{id}").and(accept(APPLICATION_JSON)), handler::getPerson)
+	// .andRoute(GET("/person").and(accept(APPLICATION_JSON)), handler::listPeople)
+	// .andRoute(POST("/person"), handler::createPerson)
+	// .andRoute(GET("/getJsonPerson/{id}").and(accept(APPLICATION_JSON)),
+	// handler::getJsonPerson);
+	// return route;
+	// }
+	//
+	// @Bean
+	// public RouterFunction<?> routerFunctionB() {
+	// // ...
+	// }
+
+	// ...
+
+	@Override
+	public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+		// configure message conversion...
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// configure CORS...
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		// configure view resolution for HTML rendering...
+	}
+}
