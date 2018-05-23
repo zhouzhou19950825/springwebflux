ae4b607464aeb78fe63183039af17a12198a5326
zhouzhou19950825 <729535800@qq.com>
2018-05-24 07:59:54 +0800

    config update

--- upic-spring-test/src/main/java/com/upic/config/WebConfig.java
+++ upic-spring-test/src/main/java/com/upic/config/WebConfig.java
@@ -1,13 +1,17 @@
 package com.upic.config;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.CacheControl;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.validation.Validator;
 import org.springframework.web.reactive.accept.RequestedContentTypeResolverBuilder;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.PathMatchConfigurer;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 import org.springframework.web.reactive.config.ViewResolverRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
@@ -79,11 +83,23 @@ public class WebConfig implements WebFluxConfigurer {
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 	}
+
 	/**
 	 * 文档configurePathMatch错误，应修改configurePathMatching
+	 * setUseCaseSensitiveMatch 是否区分大小写 默认false (为false的时候不管大小写)
+	 * setUseTrailingSlashMatch 是否区分尾斜线 默认true
 	 */
 	@Override
-    public void configurePathMatching(PathMatchConfigurer configurer) {
-        // ...
-    }
+	public void configurePathMatching(PathMatchConfigurer configurer) {
+		// ...
+		configurer.setUseCaseSensitiveMatch(false);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/public", "classpath:/static/")
+				.setCacheControl(CacheControl.maxAge(31556926, TimeUnit.SECONDS));
+		// 这个方法不存在，spring webflux文档中的bug
+		// .setCachePeriod(31556926);
+	}
 }
